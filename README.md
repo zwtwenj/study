67605bcb2c85d7a976bc759bac03a69a3f312b37
zhouwenjie <zhouwenjie@supconit.com>
2023-06-12 09:36:21 +0800

    11

--- README.md
+++ README.md
@@ -1,12 +1,1 @@
 个人学习笔记
-打卡
-5.14    qiankun使用
-5.15    qiankun使用
-5.16    qiankun原理学习
-5.17    qiankun原理学习
-5.18    王国之泪真好玩
-5.19    qiankun原理学习
-5.23    sentry配置
-5.26    研究一下低代码
-6.6     最近阳了，整理一下sentry的数据收集
-6.7     看了下three和nginx反向代理
--- three/my-three/src/App.vue
+++ three/my-three/src/App.vue
@@ -1,39 +1,40 @@
 <template>
-  <div></div>
+    <div></div>
 </template>
 
 <script setup>
-import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh } from 'three'
+import { Scene, PerspectiveCamera, WebGLRenderer, BoxGeometry, MeshBasicMaterial, Mesh, LineBasicMaterial, Vector3, BufferGeometry, Line } from 'three'
 // 场景
 const scene = new Scene()
 // 相机
-const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
+const camera = new PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 500)
 // 渲染器
 const renderer = new WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
-const geometry = new BoxGeometry( 1, 1, 1 );
-const material = new MeshBasicMaterial( { color: 0x00ff00 } );
-const cube = new Mesh( geometry, material );
-scene.add( cube );
+// const geometry = new BoxGeometry( 1, 1, 1 );
+// const material = new MeshBasicMaterial( { color: '0x00ff00' } );
+// const cube = new Mesh( geometry, material );
+// scene.add( cube );
+// camera.position.z = 5;
 
-camera.position.z = 5;
+const material = new LineBasicMaterial( { color: 0x00ff00 } );
+const points = [];
+points.push(new Vector3( - 10, 0, 0 ));
+points.push(new Vector3( 0, 10, 0 ));
+points.push(new Vector3( 10, 0, 0 ));
+const geometry = new BufferGeometry().setFromPoints( points );
+const line = new Line( geometry, material )
+scene.add( line )
 
-const animate = () => {
-    requestAnimationFrame( animate );
-
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-
-    renderer.render( scene, camera );
-};
-animate()
+renderer.render( scene, camera )
 </script>
 
 <style>
 *{
-  margin: 0;
-  padding: 0;
+margin: 0;
+padding: 0;
 }
 </style>
+  
